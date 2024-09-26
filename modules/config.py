5e99c1a371c70520f1d67d63fe4a729101be2524
pinakm9 <pinakm9@gmail.com>
2024-09-26 19:37:32 +1000

    test-16384

--- modules/config.py
+++ modules/config.py
@@ -155,13 +155,13 @@ def config_test(dynamical_system):
 
 def config_test_1(dynamical_system):
     if dynamical_system == 'KS':
-        prediction_time_config = {"error_threshold": 0.25, "dt": 0.25, "Lyapunov_time": 1/0.04}
-        train_test_config = {"training_points": int(4.5e4), "n_repeats": 100}
-        data_gen_config = {"dt": prediction_time_config["dt"], "train_seed": 22, "train_size": 2*train_test_config["training_points"],\
+        prediction_time_config = {"error_threshold": 0.25, "dt": 0.25, "Lyapunov_time": 1/0.043}
+        train_test_config = {"training_points": int(1e5), "n_repeats": 100}
+        data_gen_config = {"dt": prediction_time_config["dt"], "train_seed": 22, "train_size": train_test_config["training_points"],\
                            "test_seed": 43, "test_num": train_test_config["n_repeats"], "test_size": 1000, "save_folder": None}
-        beta_config = {"negative_log10_range": [-1, 8], "resolution":25, "n_repeats": 2,\
+        beta_config = {"negative_log10_range": [-1, 8], "resolution":50, "n_repeats": 2,\
                        "training_points": train_test_config["training_points"]}
-        beta_arch_config = {"LocalSkip_8_1": [[2048, 1]]}
+        beta_arch_config = {"LocalSkip_8_1": [[16384, 1]]}
         arch_configs = [{"LocalSkip_8_1": [[512, 1], [1024, 1], [2048, 1], [4096, 1], [8192, 1], [16384, 1]],\
                          "LocalDeepSkip_8_1":[[1024, 1], [1024, 2], [1024, 4], [1024, 8], [1024, 16]]}]
         return {"prediction_time": prediction_time_config, "train_test": train_test_config, "data_gen": data_gen_config,\
