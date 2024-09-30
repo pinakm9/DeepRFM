5be4ee729851a2d0a57422dd29d93a5d38c6c65d
pinakm9 <pinakm9@gmail.com>
2024-09-30 16:11:00 +1000

    12-1

--- modules/config.py
+++ modules/config.py
@@ -153,7 +153,23 @@ def config_test(dynamical_system):
     
 
 
-def config_test_1(dynamical_system):
+def config_test_12(dynamical_system):
+    if dynamical_system == 'KS':
+        prediction_time_config = {"error_threshold": 0.25, "dt": 0.25, "Lyapunov_time": 1/0.003}
+        train_test_config = {"training_points": int(1e5), "n_repeats": 100}
+        data_gen_config = {"dt": prediction_time_config["dt"], "train_seed": 22, "train_size": train_test_config["training_points"],\
+                           "test_seed": 43, "test_num": 2, "test_size": int(7.5e4), "save_folder": None}
+        beta_config = {"negative_log10_range": [-1, 8], "resolution":25, "n_repeats": 5,\
+                       "training_points": train_test_config["training_points"]}
+        beta_arch_config = {"SkipRFM": [[2048, 1]],\
+                            "LocalSkip_12_1": [[2048, 1]]}
+        arch_configs = [{"LocalSkip_12_1": [[512, 1], [1024, 1], [2048, 1], [4096, 1]],\
+                         "LocalDeepSkip_12_1":[[1024, 1], [1024, 2], [1024, 4], [1024, 8], [1024, 16]]}]
+        return {"prediction_time": prediction_time_config, "train_test": train_test_config, "data_gen": data_gen_config,\
+                 "beta": beta_config, "beta_arch": beta_arch_config, "arch": arch_configs}
+    
+
+def config_test_22(dynamical_system):
     if dynamical_system == 'KS':
         prediction_time_config = {"error_threshold": 0.25, "dt": 0.25, "Lyapunov_time": 1/0.043}
         train_test_config = {"training_points": int(1e5), "n_repeats": 100}
@@ -161,7 +177,22 @@ def config_test_1(dynamical_system):
                            "test_seed": 43, "test_num": train_test_config["n_repeats"], "test_size": 1000, "save_folder": None}
         beta_config = {"negative_log10_range": [-1, 8], "resolution":50, "n_repeats": 2,\
                        "training_points": train_test_config["training_points"]}
-        beta_arch_config = {"LocalDeepSkip_8_1": [[1024, 8]]}
+        beta_arch_config = {"LocalSkip_16_1": [[1024, 8]]}
+        arch_configs = [{"LocalSkip_8_1": [[512, 1], [1024, 1], [2048, 1], [4096, 1], [8192, 1], [16384, 1]],\
+                         "LocalDeepSkip_8_1":[[1024, 1], [1024, 2], [1024, 4], [1024, 8], [1024, 16]]}]
+        return {"prediction_time": prediction_time_config, "train_test": train_test_config, "data_gen": data_gen_config,\
+                 "beta": beta_config, "beta_arch": beta_arch_config, "arch": arch_configs}
+    
+    
+def config_test_200(dynamical_system):
+    if dynamical_system == 'KS':
+        prediction_time_config = {"error_threshold": 0.25, "dt": 0.25, "Lyapunov_time": 1/0.094}
+        train_test_config = {"training_points": int(1e5), "n_repeats": 100}
+        data_gen_config = {"dt": prediction_time_config["dt"], "train_seed": 22, "train_size": train_test_config["training_points"],\
+                           "test_seed": 43, "test_num": train_test_config["n_repeats"], "test_size": 1000, "save_folder": None}
+        beta_config = {"negative_log10_range": [-1, 8], "resolution":25, "n_repeats": 4,\
+                       "training_points": train_test_config["training_points"]}
+        beta_arch_config = {"LocalSkip_8_1": [[2048, 1]]}
         arch_configs = [{"LocalSkip_8_1": [[512, 1], [1024, 1], [2048, 1], [4096, 1], [8192, 1], [16384, 1]],\
                          "LocalDeepSkip_8_1":[[1024, 1], [1024, 2], [1024, 4], [1024, 8], [1024, 16]]}]
         return {"prediction_time": prediction_time_config, "train_test": train_test_config, "data_gen": data_gen_config,\
--- modules/localSkip_12_1.py
+++ modules/localSkip_12_1.py
@@ -11,19 +11,19 @@ from torch import nn
 import rfm
 
 
-class LocalSkip_16_1(nn.Module):
+class LocalSkip_12_1(nn.Module):
     def __init__(self, D, D_r, B):
         super().__init__()
         self.D = D
         self.D_r = D_r
         self.B = B
-        self.G = 16
+        self.G = 12 
         self.I = 1
         self.Ng = int(self.D / self.G)
         self.idx = torch.arange(-self.I*self.G, (self.I+1)*self.G) % D
-        self.idx = torch.vstack([(self.idx + i*self.G) % D for i in range(self.Ng)])
+        self.idx = torch.vstack([(self.idx + self.G*i) % D for i in range(self.Ng)])
         self.idy = torch.arange(0, self.G)
-        self.idy = torch.vstack([(self.idy + i*self.G) % D for i in range(self.Ng)])
+        self.idy = torch.vstack([(self.idy + self.G*i) % D for i in range(self.Ng)])
         self.inner = nn.ModuleList([nn.Linear((2*self.I + 1)*self.G, self.D_r, bias=True)])
         self.outer = nn.ModuleList([nn.Linear(self.D_r, self.G, bias=False)])
 
@@ -45,7 +45,7 @@ class DeepRF(rfm.DeepRF):
             beta: regularization parameter
         """        
         super().__init__(D_r, B, L0, L1, Uo, beta, name, save_folder, normalize)
-        self.net = LocalSkip_16_1(self.sampler.dim, D_r, B)
+        self.net = LocalSkip_12_1(self.sampler.dim, D_r, B)
         self.net.to(self.device)
         self.logger.update(start=False, kwargs={'parameters': self.count_params()})
         self.sampler.update((Uo.T[:-1][..., self.net.idx]).flatten(0, 1).T)
@@ -57,13 +57,25 @@ class DeepRF(rfm.DeepRF):
         X = (train.T[:-1][..., self.net.idx]).flatten(0, 1).T
         Y = (train.T[1:][..., self.net.idy] - train.T[:-1][..., self.net.idy]).flatten(0, 1).T
         indices = torch.randperm(X.shape[1])
-        X = X[:, indices[:train.shape[1]]]
-        Y = Y[:, indices[:train.shape[1]]]
+        X = X[:, indices[:train.shape[-1]]]
+        Y = Y[:, indices[:train.shape[-1]]]
         with torch.no_grad():
             Wb = self.sampler.sample_vec(self.net.D_r, seed=seed)
             self.net.inner[0].weight = nn.Parameter(Wb[:, :-1])
             self.net.inner[0].bias = nn.Parameter(Wb[:, -1])
             self.net.outer[0].weight = nn.Parameter(self.compute_W(Wb, X, Y))
+    
+    def learn_(self, train, seed):
+        X = (train.T[:-1][..., self.net.idx])
+        Y = (train.T[1:][..., self.net.idy] - train.T[:-1][..., self.net.idy])
+        Wb, W = 0., 0.
+        with torch.no_grad():
+            for i in range(self.net.Ng):
+                Wb += self.sampler.sample_vec(self.net.D_r, seed=seed)
+                W  += self.compute_W(Wb, X[:, i, :].T, Y[:, i, :].T)
+            self.net.inner[0].weight = nn.Parameter(Wb[:, :-1] / self.net.Ng)
+            self.net.inner[0].bias = nn.Parameter(Wb[:, -1] / self.net.Ng)
+            self.net.outer[0].weight = nn.Parameter(W / self.net.Ng)   
 
 
 class BatchDeepRF(rfm.BatchDeepRF):
