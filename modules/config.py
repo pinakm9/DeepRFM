e666aa629ea2dd95de2224449bea0652f945d614
pinakm9 <pinakm9@gmail.com>
2024-10-03 17:12:16 +1000

    g=4

--- modules/config.py
+++ modules/config.py
@@ -219,3 +219,20 @@ def config_local(dynamical_system):
                          "LocalDeepSkip_2_2":[[1024, 1], [1024, 2], [1024, 4], [1024, 8], [1024, 16], [4096, 2], [4096, 4], [4096, 8], [8192, 2], [8192, 4], [16384, 2]]}]
         return {"prediction_time": prediction_time_config, "train_test": train_test_config, "data_gen": data_gen_config,\
                  "beta": beta_config, "beta_arch": beta_arch_config, "arch": arch_configs}
+    
+    elif dynamical_system == 'KS-22':
+        prediction_time_config = {"error_threshold": 0.25, "dt": 0.25, "Lyapunov_time": 1/0.043}
+        train_test_config = {"training_points": int(1e5), "n_repeats": 100}
+        data_gen_config = {"dt": prediction_time_config["dt"], "train_seed": 22, "train_size": train_test_config["training_points"],\
+                           "test_seed": 43, "test_num": train_test_config["n_repeats"], "test_size": 1000, "save_folder": None}
+        beta_config = {"negative_log10_range": [-1, 8], "resolution":50, "n_repeats": 2,\
+                       "training_points": train_test_config["training_points"]}
+        beta_arch_config = {"LocalSkip":  [[[4096, 1, 1, 1], [4096, 1, 1, 2], [4096, 1, 1, 3], [4096, 1, 1, 4], [4096, 1, 1, 5], [4096, 1, 1, 6], [4096, 1, 1, 7], [4096, 1, 1, 8]],\
+                                           [[4096, 1, 2, 1], [4096, 1, 2, 2], [4096, 1, 2, 3], [4096, 1, 2, 4], [4096, 1, 2, 5], [4096, 1, 2, 6], [4096, 1, 2, 7], [4096, 1, 2, 8]],\
+                                           [[4096, 1, 4, 1], [4096, 1, 4, 2], [4096, 1, 4, 3], [4096, 1, 4, 4], [4096, 1, 4, 5], [4096, 1, 4, 6], [4096, 1, 4, 7], [4096, 1, 4, 8]],\
+                                           [[4096, 1, 8, 1], [4096, 1, 8, 2], [4096, 1, 8, 3], [4096, 1, 8, 4], [4096, 1, 8, 5], [4096, 1, 8, 6], [4096, 1, 8, 7], [4096, 1, 8, 8]]]}
+        arch_configs = [{"LocalSkip_8_1": [[512, 1], [1024, 1], [2048, 1], [4096, 1], [8192, 1], [16384, 1]],\
+                         "LocalDeepSkip_8_1":[[1024, 1], [1024, 2], [1024, 4], [1024, 8], [1024, 16]]}]
+        return {"prediction_time": prediction_time_config, "train_test": train_test_config, "data_gen": data_gen_config,\
+                 "beta": beta_config, "beta_arch": beta_arch_config, "arch": arch_configs}
+    
--- modules/rfm.py
+++ modules/rfm.py
@@ -255,6 +255,9 @@ class BatchDeepRF:
         self.drf_args = list(drf_args)
         if isinstance(self.drf_args[-1], int) and isinstance(self.drf_args[-2], int):
             self.drf_args[-4] = self.drf_args[-4][:-5] + f'_{self.drf_args[-2]}_{self.drf_args[-1]}' + self.drf_args[-4][-5:]
+            self.local = True 
+        else:
+            self.local = False
         self.drf_type = drf_type
         self.drf = self.drf_type(*self.drf_args)
         self.std = torch.std(train, axis=1)
@@ -405,7 +408,10 @@ class BatchDeepRF:
             pd.DataFrame(results, columns=columns_agg, dtype=float)\
                         .to_csv(file_path_agg, mode='a', index=False, header=not os.path.exists(file_path_agg))
             end = time.time()
-            print(f'Experiments for (D_r, B, beta) = ({self.drf_args[0]}, {self.drf_args[1]}, {beta:.2E}) took {end-start:.2E}s')
+            if not self.local:
+                print(f'Experiments for (D_r, B, beta) = ({self.drf_args[0]}, {self.drf_args[1]}, {beta:.2E}) took {end-start:.2E}s')
+            else:
+                print(f'Experiments for (D_r, B, G, I, beta) = ({self.drf_args[0]}, {self.drf_args[1]}, {self.drf_args[-2]}, {self.drf_args[-1]}, {beta:.2E}) took {end-start:.2E}s')
             k += n_repeats
             if self.drf.device == 'cuda':
                 torch.cuda.empty_cache()
