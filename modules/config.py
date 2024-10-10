529cff8de3f5d9bade29db4999c66c29dde3dbde
pinakm9 <pinakm9@gmail.com>
2024-10-10 11:34:19 +1100

    localRFM

--- modules/config.py
+++ modules/config.py
@@ -245,3 +245,19 @@ def config_local(dynamical_system):
         return {"prediction_time": prediction_time_config, "train_test": train_test_config, "data_gen": data_gen_config,\
                  "beta": beta_config, "beta_arch": beta_arch_config, "arch": arch_configs}
     
+    elif dynamical_system == 'KS-200':
+        prediction_time_config = {"error_threshold": 0.25, "dt": 0.25, "Lyapunov_time": 1/0.094}
+        train_test_config = {"training_points": int(1e5), "n_repeats": 100}
+        data_gen_config = {"dt": prediction_time_config["dt"], "train_seed": 22, "train_size": train_test_config["training_points"],\
+                           "test_seed": 43, "test_num": train_test_config["n_repeats"], "test_size": 1000, "save_folder": None}
+        beta_config = {"negative_log10_range": [-1, 8], "resolution":25, "n_repeats": 5,\
+                       "training_points": train_test_config["training_points"]}
+        beta_arch_config = {"LocalRFM":  [[8192, 1, 1, 1],\
+                                           [8192, 1, 2, 1],\
+                                           [8192, 1, 4, 1],\
+                                           [8192, 1, 8, 1]]}
+        arch_configs = [{"LocalSkip_8_1": [[512, 1], [1024, 1], [2048, 1], [8192, 1], [8192, 1], [16384, 1]],\
+                         "LocalDeepSkip_8_1":[[1024, 1], [1024, 2], [1024, 4], [1024, 8], [1024, 16]]}]
+        return {"prediction_time": prediction_time_config, "train_test": train_test_config, "data_gen": data_gen_config,\
+                 "beta": beta_config, "beta_arch": beta_arch_config, "arch": arch_configs}
+    
