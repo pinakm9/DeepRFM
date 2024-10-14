d3fb2c4d2e2d1432dfa91be721506d104549d99f
pinakm9 <pinakm9@gmail.com>
2024-10-14 16:39:40 +1100

    config

--- modules/config.py
+++ modules/config.py
@@ -50,7 +50,7 @@ def config_1_s(dynamical_system):
         beta_config = {"negative_log10_range": [4, 7], "resolution":25, "n_repeats": 5,\
                        "training_points": train_test_config["training_points"]}
         beta_arch_config = {"LocalDeepRFM":  [[11586, 1, 8, 1], [8192, 1, 8, 1], [4096, 1, 8, 1], [2048, 1, 8, 1]],
-                            "LocalDeepSkip":  [[11586], [8192, 1, 8, 1], [4096, 1, 8, 1], [2048, 1, 8, 1]]}
+                            "LocalDeepSkip":  [[11586, 1, 8, 1], [8192, 1, 8, 1], [4096, 1, 8, 1], [2048, 1, 8, 1]]}
         arch_configs = [{"LocalRFM":  [[14000, 1, 4, 2]]}]
         return {"prediction_time": prediction_time_config, "train_test": train_test_config, "data_gen": data_gen_config,\
                  "beta": beta_config, "beta_arch": beta_arch_config, "arch": arch_configs}
