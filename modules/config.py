07492db7c0fa0935fe9fba1af1fdce193ff645c5
pinakm9 <pinakm9@gmail.com>
2024-10-16 12:53:03 +1100

    4.7

--- modules/config.py
+++ modules/config.py
@@ -61,7 +61,7 @@ def config_1(dynamical_system):
         beta_config = {"negative_log10_range": [3, 7], "resolution":25, "n_repeats": 5,\
                        "training_points": train_test_config["training_points"]}
         beta_arch_config = {"LocalDeepRFM":  [[11586, 1, 8, 1]]}
-        arch_configs = [{"LocalDeepRFM":  [[13308, 3, 8, 1]]}]
+        arch_configs = [{"LocalDeepRFM":  [[15000, 3, 8, 1]]}]
         return {"prediction_time": prediction_time_config, "train_test": train_test_config, "data_gen": data_gen_config,\
                  "beta": beta_config, "beta_arch": beta_arch_config, "arch": arch_configs}
 
---
+++ modules/localDeepRFMFull.py
@@ -0,0 +1,82 @@
+# load necessary modules
+import sys
+from pathlib import Path
+from os.path import dirname, realpath
+script_dir = Path(dirname(realpath('.')))
+module_dir = str(script_dir)
+sys.path.insert(0, module_dir + '/modules')
+import utility as ut
+import torch
+from torch import nn
+import rfm
+
+
+class LocalDeepRFMFull(nn.Module):
+    def __init__(self, D, D_r, B, G, I):
+        super().__init__()
+        self.D = D
+        self.D_r = D_r
+        self.B = B
+        self.G = G
+        self.I = I 
+        self.Ng = int(self.D / self.G)
+        self.idx = torch.arange(-self.I*self.G, (self.I+1)*self.G) % D
+        self.idx = torch.vstack([(self.idx + i*self.G) % D for i in range(self.Ng)])
+        self.idy = torch.arange(0, self.G)
+        self.idy = torch.vstack([(self.idy + i*self.G) % D for i in range(int(self.Ng))])
+        self.p = (2*self.I + 1)*self.G
+        self.inner = nn.ModuleList([nn.Linear(2*(2*self.I + 1)*self.G, self.D_r, bias=True) for _ in range(B)])
+        self.outer = nn.ModuleList([nn.Linear(self.D_r, self.G, bias=False) for _ in range(B)])
+
+    # @ut.timer  
+    def forward(self, x):
+        z = x + 0.
+        for i in range(self.B):
+            y = torch.concat((x[..., self.idx], z[..., self.idx]), dim=-1)
+            z = self.outer[i](torch.tanh(self.inner[i](y)))
+            z = z.flatten(-2, -1)
+        return z 
+    
+class DeepRF(rfm.DeepRF):
+    def __init__(self, D_r, B, L0, L1, Uo, beta, name='nn', save_folder='.', normalize=False, G=2, I=2, *args):
+        """
+        Args:
+            D_r: dimension of the feature 
+            B: number of RF blocks
+            name: name of the DeepRF
+            L0: left limit of tanh input for defining good rows
+            L1: right limit tanh input for defining good rows
+            Uo: training data
+            beta: regularization parameter
+        """        
+        super().__init__(D_r, B, L0, L1, Uo, beta, name, save_folder, normalize)
+        self.net = LocalDeepRFMFull(self.sampler.dim, D_r, B, G, I)
+        self.net.to(self.device)
+        self.logger.update(start=False, kwargs={'parameters': self.count_params()})
+        x = (Uo.T[..., self.net.idx]).flatten(0, 1).T
+        self.sampler.update(torch.vstack((x, x)))
+        self.arch = self.net.__class__
+
+
+    # @ut.timer
+    def learn(self, train, seed):
+
+        X = train.T[:-1][..., self.net.idx][:, self.net.Ng//2, :]
+        Y = train.T[1:][..., self.net.idy][:, self.net.Ng//2, :].T
+        Z = X + 0.
+        XZ = torch.concat((X, Z), dim=-1)
+
+        with torch.no_grad():
+            for i in range(self.net.B):
+                Wb = self.sampler.sample_vec(self.net.D_r, seed=seed)
+                self.net.inner[i].weight = nn.Parameter(Wb[:, :-1])
+                self.net.inner[i].bias = nn.Parameter(Wb[:, -1])
+                self.net.outer[i].weight = nn.Parameter(self.compute_W(Wb, XZ.T, Y))
+                Z = self.net.outer[i](torch.tanh(self.net.inner[i](XZ)))
+                XZ = torch.concat((X, Z), dim=-1)
+
+
+
+class BatchDeepRF(rfm.BatchDeepRF):
+    def __init__(self, train, test, *drf_args):
+        super().__init__(DeepRF, train, test, *drf_args) 
