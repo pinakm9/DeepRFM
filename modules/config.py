f0ed51071df34dfe07f71900bf4e01ed57b595da
pinakm9 <pinakm9@gmail.com>
2024-10-17 21:14:52 +1100

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -144,7 +144,7 @@ def config_0(dynamical_system):
     
     elif dynamical_system == 'KS-200':
         prediction_time_config = {"error_threshold": 0.25, "dt": 0.25, "Lyapunov_time": 1/0.094}
-        train_test_config = {"training_points": int(1e5), "n_repeats": 100}
+        train_test_config = {"training_points": int(1e5), "n_repeats": 500}
         data_gen_config = {"dt": prediction_time_config["dt"], "train_seed": 22, "train_size": 2*train_test_config["training_points"],\
                            "test_seed": 43, "test_num": train_test_config["n_repeats"], "test_size": 1000, "save_folder": None}
         beta_config = {"negative_log10_range": [4, 6], "resolution":50, "n_repeats": 2,\
