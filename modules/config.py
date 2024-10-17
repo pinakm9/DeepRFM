7098e3e4390c0a185d18315490034bea00ddba47
pinakm9 <pinakm9@gmail.com>
2024-10-17 16:39:23 +1100

    n

--- modules/config.py
+++ modules/config.py
@@ -147,7 +147,7 @@ def config_0(dynamical_system):
         train_test_config = {"training_points": int(1e5), "n_repeats": 500}
         data_gen_config = {"dt": prediction_time_config["dt"], "train_seed": 22, "train_size": 2*train_test_config["training_points"],\
                            "test_seed": 43, "test_num": train_test_config["n_repeats"], "test_size": 1000, "save_folder": None}
-        beta_config = {"negative_log10_range": [4, 5], "resolution":20, "n_repeats": 2,\
+        beta_config = {"negative_log10_range": [4, 5], "resolution":50, "n_repeats": 4,\
                        "training_points": train_test_config["training_points"]}
         beta_arch_config = {"LocalDeepRFM":  [[14000, 3, 8, 1]]}
         arch_configs = [{"LocalDeepRFM":  [[14000, 3, 8, 1]]}]
