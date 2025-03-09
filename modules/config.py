2e784cb118ffb1f798e2fa0ad4c021ff89a111ab
pinakm9 <pinakm9@gmail.com>
2025-03-10 10:31:53 +1100

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -6,9 +6,7 @@ def config_1_s(dynamical_system):
                            "test_seed": 43, "test_num": train_test_config["n_repeats"], "test_size": 2500, "save_folder": None}
         beta_config = {"negative_log10_range": [6, 11], "resolution":25, "n_repeats": 5,\
                        "training_points": train_test_config["training_points"]}
-        beta_arch_config = {"RFM": [[512, 1, None, None], [1024, 1, None, None], [2048, 1, None, None], [4096, 1, None, None], [8192, 1, None, None], [16384, 1, None, None]],\
-                            "SkipRFM": [[512, 1, None, None], [1024, 1, None, None], [2048, 1, None, None], [4096, 1, None, None], [8192, 1, None, None], [16384, 1, None, None]],\
-                            "DeepRFM":[[512, 1, None, None], [1024, 1, None, None], [2048, 1, None, None], [4096, 1, None, None], [8192, 1, None, None], [16384, 1, None, None]]}
+        beta_arch_config = {"DeepRFM":[[512, 1, None, None], [1024, 1, None, None], [2048, 1, None, None], [4096, 1, None, None], [8192, 1, None, None], [16384, 1, None, None]]}
         arch_configs = [{"RFM": [[512, 1, None, None], [1024, 1, None, None], [2048, 1, None, None], [4096, 1, None, None], [8192, 1, None, None], [16384, 1, None, None]],\
                          "SkipRFM": [[512, 1, None, None], [1024, 1, None, None], [2048, 1, None, None], [4096, 1, None, None], [8192, 1, None, None], [16384, 1, None, None]],\
                          "DeepSkip":[[1024, 1, None, None], [1024, 2, None, None], [1024, 4, None, None], [1024, 8, None, None], [1024, 16, None, None], [1024, 32, None, None],\
